ab9083aa92e14a76a7b9041b87d4ae04d5a0eea1
AlexHaxe <Alexander.Blum@gmail.com>
2018-11-21 11:53:07 +0100

    try both lcov and codecov json

--- test/TestMain.hx
+++ test/TestMain.hx
@@ -1,19 +1,14 @@
 import haxe.EntryPoint;
-import haxe.Json;
 import sys.io.File;
-import sys.io.FileOutput;
 import massive.munit.TestRunner;
 import mcover.coverage.munit.client.MCoverPrintClient;
-import mcover.coverage.data.CoverageResult;
-import mcover.coverage.data.Statement;
-import mcover.coverage.data.Branch;
 import mcover.coverage.MCoverage;
 
 class TestMain {
 	public function new() {
 		var suites:Array<Class<massive.munit.TestSuite>> = [TestSuite];
 		var client:MCoverPrintClient = new MCoverPrintClient();
-		// MCoverage.getLogger().addClient(new LcovPrintClient("Formatter Unittests"));
+		MCoverage.getLogger().addClient(new LcovPrintClient("Formatter Unittests"));
 		MCoverage.getLogger().addClient(new CodecovJsonPrintClient());
 
 		#if detailed_coverage
@@ -31,13 +26,9 @@ class TestMain {
 		#end
 		runner.run(suites);
 		EntryPoint.run();
-		#if eval
-		// setupCoverageReport();
-		#end
 	}
 
 	function completionHandler(success:Bool) {
-		// setupCoverageReport();
 		if (success) {
 			File.saveContent("test/formatter-result.txt", "\n---\n");
 		}
@@ -50,57 +41,6 @@ class TestMain {
 		#end
 	}
 
-	// function setupCoverageReport() {
-	// 	var report = {coverage: {}};
-	// 	var classes = MCoverage.getLogger().coverage.getClasses();
-	// 	for (cls in classes) {
-	// 		var coverageData:Array<LineCoverageResult> = [null];
-	// 		var results:CoverageResult = cls.getResults();
-	// 		trace(cls.name + " " + results);
-	// 		for (i in 1...results.l) {
-	// 			coverageData[i] = null;
-	// 		}
-	// 		var c = cls.name.replace(".", "/") + ".hx";
-	// 		var missingStatements:Array<Statement> = cls.getMissingStatements();
-	// 		// trace(missingStatements);
-	// 		var maxLine:Int = results.l;
-	// 		for (stmt in missingStatements) {
-	// 			// trace(stmt.name + " " + stmt.lines);
-	// 			for (line in stmt.lines) {
-	// 				if (line > maxLine) {
-	// 					maxLine = line;
-	// 				}
-	// 				coverageData[line] = 0;
-	// 			}
-	// 		}
-	// 		var missingBranches:Array<Branch> = cls.getMissingBranches();
-	// 		for (branch in missingBranches) {
-	// 			// trace(branch.name + " " + branch.lines);
-	// 			for (line in branch.lines) {
-	// 				if (line > maxLine) {
-	// 					maxLine = line;
-	// 				}
-	// 				// if (branch.lines.length <= 0) {
-	// 				// 	continue;
-	// 				// }
-	// 				var count:Int = 0;
-	// 				if (branch.trueCount > 0) {
-	// 					count++;
-	// 				}
-	// 				if (branch.falseCount > 0) {
-	// 					count++;
-	// 				}
-	// 				// var line:Int = branch.lines[branch.lines.length - 1];
-	// 				coverageData[line] = count + "/2";
-	// 			}
-	// 		}
-	// 		trace(cls.name + " " + results.l + " " + maxLine);
-	// 		Reflect.setField(report.coverage, c, coverageData);
-	// 	}
-	// 	var file:FileOutput = File.write("coverage.json");
-	// 	file.writeString(Json.stringify(report));
-	// 	file.close();
-	// }
 	static function main() {
 		new TestMain();
 	}
