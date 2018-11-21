dbd8593b04188d0e4f86a967806ecd297c0567d8
AlexHaxe <Alexander.Blum@gmail.com>
2018-11-21 19:33:13 +0100

    moved lcov and codecov print clients to mcover branch

--- test/TestMain.hx
+++ test/TestMain.hx
@@ -2,6 +2,11 @@ import haxe.EntryPoint;
 import sys.io.File;
 import massive.munit.TestRunner;
 import mcover.coverage.munit.client.MCoverPrintClient;
+#if codecov_json
+import mcover.coverage.client.CodecovJsonPrintClient;
+#else
+import mcover.coverage.client.LcovPrintClient;
+#end
 import mcover.coverage.MCoverage;
 
 class TestMain {
