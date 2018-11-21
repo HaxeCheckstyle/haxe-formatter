1f6a686c5440853c85389ef68137a585bdac63d5
AlexHaxe <Alexander.Blum@gmail.com>
2018-11-21 11:24:47 +0100

    try out new lcov print client

---
+++ test/LcovPrintClient.hx
@@ -0,0 +1,118 @@
+import sys.io.FileOutput;
+import mcover.coverage.CoverageReportClient;
+import mcover.coverage.DataTypes;
+
+class LcovPrintClient implements CoverageReportClient {
+	public static inline var LCOV_INFO:String = "lcov.info";
+
+	public var completionHandler(default, default):CoverageReportClient->Void;
+	public var output(default, null):String;
+
+	var testName:String;
+
+	public function new(name:String) {
+		testName = name;
+	}
+
+	public function report(coverage:Coverage) {
+		sys.io.File.saveContent(LCOV_INFO, makeLine("TN", testName) + "\n");
+
+		for (cls in coverage.getClasses()) {
+			reportClass(cls);
+		}
+		if (completionHandler != null) {
+			completionHandler(this);
+		}
+	}
+
+	function reportClass(cls:Clazz) {
+		var results:CoverageResult = cls.getResults();
+		var c = cls.name.replace(".", "/") + ".hx";
+		var text:StringBuf = new StringBuf();
+		text.add(makeLine("SF", c));
+		text.add(makeLine("FNF", '${results.m}'));
+		text.add(makeLine("FNH", '${results.mc}'));
+		text.add("\n");
+		text.add(makeLine("BRF", '${results.b}'));
+		text.add(makeLine("BRH", '${results.bt}'));
+		text.add("\n");
+
+		var lineCov:Map<Int, Int> = new Map<Int, Int>();
+		var maxLineNumber:Int = 0;
+		for (method in cls.getMethods()) {
+			var max:Int = reportMethod(text, method, lineCov);
+			if (max > maxLineNumber) {
+				maxLineNumber = max;
+			}
+		}
+
+		for (line in 0...maxLineNumber + 1) {
+			if (!lineCov.exists(line)) {
+				continue;
+			}
+			var count:Int = lineCov.get(line);
+			text.add(makeLine("DA", '${line},${count}'));
+		}
+
+		text.add(makeLine("LF", '${results.l}'));
+		text.add(makeLine("LH", '${results.lc}'));
+		text.add("\n");
+
+		text.add("end_of_record\n\n");
+		appendCoverageFile(text.toString());
+	}
+
+	@:access(mcover.coverage.data.Method)
+	function reportMethod(text:StringBuf, method:Method, lineCov:Map<Int, Int>):Int {
+		var maxLineNumber:Int = 0;
+
+		// TODO get first line number of method
+		text.add(makeLine("FN", '1,${method.name}'));
+
+		for (statementId in method.statementsById.keys()) {
+			var statement:Statement = method.statementsById.get(statementId);
+			for (line in statement.lines) {
+				maxLineNumber = addLineCov(line, lineCov, statement.count, maxLineNumber);
+			}
+		}
+		for (branchId in method.branchesById.keys()) {
+			var branch:Branch = method.branchesById.get(branchId);
+			if (branch.isCovered()) {
+				for (line in branch.lines) {
+					maxLineNumber = addLineCov(line, lineCov, 1, maxLineNumber);
+				}
+			} else {
+				for (line in branch.lines) {
+					text.add(makeLine("BRDA", '$line,${method.id},${branch.id},-'));
+					maxLineNumber = addLineCov(line, lineCov, branch.totalCount, maxLineNumber);
+				}
+			}
+		}
+		return maxLineNumber;
+	}
+
+	function addLineCov(line:Int, lineCov:Map<Int, Int>, count:Int, maxLineNumber:Int):Int {
+		if (line > maxLineNumber) {
+			maxLineNumber = line;
+		}
+		if (!lineCov.exists(line)) {
+			lineCov.set(line, count);
+			return maxLineNumber;
+		}
+		var old:Int = lineCov.get(line);
+		if (old > count) {
+			lineCov.set(line, count);
+		}
+		return maxLineNumber;
+	}
+
+	function appendCoverageFile(text:String) {
+		var file:FileOutput = sys.io.File.append(LCOV_INFO);
+		file.writeString(text.toString());
+		file.close();
+	}
+
+	inline function makeLine(key:String, value:String):String {
+		return '$key:$value\n';
+	}
+}
--- test/TestMain.hx
+++ test/TestMain.hx
@@ -13,6 +13,9 @@ class TestMain {
 	public function new() {
 		var suites:Array<Class<massive.munit.TestSuite>> = [TestSuite];
 		var client:MCoverPrintClient = new MCoverPrintClient();
+		MCoverage.getLogger().addClient(new LcovPrintClient("Formatter Unittests"));
+		// MCoverage.getLogger().addClient(new CodecovJsonPrintClient());
+
 		#if detailed_coverage
 		client.includeClassAndPackageBreakdowns = true;
 		client.includeMissingBlocks = true;
@@ -29,12 +32,12 @@ class TestMain {
 		runner.run(suites);
 		EntryPoint.run();
 		#if eval
-		setupCoverageReport();
+		// setupCoverageReport();
 		#end
 	}
 
 	function completionHandler(success:Bool) {
-		setupCoverageReport();
+		// setupCoverageReport();
 		if (success) {
 			File.saveContent("test/formatter-result.txt", "\n---\n");
 		}
@@ -47,44 +50,57 @@ class TestMain {
 		#end
 	}
 
-	function setupCoverageReport() {
-		var report = {coverage: {}};
-		var classes = MCoverage.getLogger().coverage.getClasses();
-		for (cls in classes) {
-			var coverageData:Array<LineCoverageResult> = [null];
-			var results:CoverageResult = cls.getResults();
-			for (i in 1...results.l) {
-				coverageData[i] = 1;
-			}
-			var c = cls.name.replace(".", "/") + ".hx";
-			var missingStatements:Array<Statement> = cls.getMissingStatements();
-			for (stmt in missingStatements) {
-				for (line in stmt.lines) {
-					coverageData[line + 1] = 0;
-				}
-			}
-			var missingBranches:Array<Branch> = cls.getMissingBranches();
-			for (branch in missingBranches) {
-				if (branch.lines.length <= 0) {
-					continue;
-				}
-				var count:Int = 0;
-				if (branch.trueCount > 0) {
-					count++;
-				}
-				if (branch.falseCount > 0) {
-					count++;
-				}
-				var line:Int = branch.lines[branch.lines.length - 1];
-				coverageData[line] = count + "/2";
-			}
-			Reflect.setField(report.coverage, c, coverageData);
-		}
-		var file:FileOutput = File.write("coverage.json");
-		file.writeString(Json.stringify(report));
-		file.close();
-	}
-
+	// function setupCoverageReport() {
+	// 	var report = {coverage: {}};
+	// 	var classes = MCoverage.getLogger().coverage.getClasses();
+	// 	for (cls in classes) {
+	// 		var coverageData:Array<LineCoverageResult> = [null];
+	// 		var results:CoverageResult = cls.getResults();
+	// 		trace(cls.name + " " + results);
+	// 		for (i in 1...results.l) {
+	// 			coverageData[i] = null;
+	// 		}
+	// 		var c = cls.name.replace(".", "/") + ".hx";
+	// 		var missingStatements:Array<Statement> = cls.getMissingStatements();
+	// 		// trace(missingStatements);
+	// 		var maxLine:Int = results.l;
+	// 		for (stmt in missingStatements) {
+	// 			// trace(stmt.name + " " + stmt.lines);
+	// 			for (line in stmt.lines) {
+	// 				if (line > maxLine) {
+	// 					maxLine = line;
+	// 				}
+	// 				coverageData[line] = 0;
+	// 			}
+	// 		}
+	// 		var missingBranches:Array<Branch> = cls.getMissingBranches();
+	// 		for (branch in missingBranches) {
+	// 			// trace(branch.name + " " + branch.lines);
+	// 			for (line in branch.lines) {
+	// 				if (line > maxLine) {
+	// 					maxLine = line;
+	// 				}
+	// 				// if (branch.lines.length <= 0) {
+	// 				// 	continue;
+	// 				// }
+	// 				var count:Int = 0;
+	// 				if (branch.trueCount > 0) {
+	// 					count++;
+	// 				}
+	// 				if (branch.falseCount > 0) {
+	// 					count++;
+	// 				}
+	// 				// var line:Int = branch.lines[branch.lines.length - 1];
+	// 				coverageData[line] = count + "/2";
+	// 			}
+	// 		}
+	// 		trace(cls.name + " " + results.l + " " + maxLine);
+	// 		Reflect.setField(report.coverage, c, coverageData);
+	// 	}
+	// 	var file:FileOutput = File.write("coverage.json");
+	// 	file.writeString(Json.stringify(report));
+	// 	file.close();
+	// }
 	static function main() {
 		new TestMain();
 	}
