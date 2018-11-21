83d67bfe8d0310b3971ad7e2da1e8074138a80cb
AlexHaxe <Alexander.Blum@gmail.com>
2018-11-21 13:32:22 +0100

    cleaner lcov output

--- test/LcovPrintClient.hx
+++ test/LcovPrintClient.hx
@@ -30,22 +30,55 @@ class LcovPrintClient implements CoverageReportClient {
 		var c = cls.name.replace(".", "/") + ".hx";
 		var text:StringBuf = new StringBuf();
 		text.add(makeLine("SF", c));
+		text.add("\n");
+
+		var lineCov:Map<Int, Int> = new Map<Int, Int>();
+		var branchCov:Map<Int, String> = new Map<Int, String>();
+		var maxLineNumber:Int = 0;
+
+		var num:Int = 0;
+		for (method in cls.getMethods()) {
+			// TODO get first line number of method
+			text.add(makeLine("FN", '${num++},${method.name}'));
+		}
+		text.add("\n");
+
+		for (method in cls.getMethods()) {
+			var methodResults:CoverageResult = method.getResults();
+			text.add(makeLine("FNDA", '${methodResults.m},${method.name}'));
+		}
+		text.add("\n");
 		text.add(makeLine("FNF", '${results.m}'));
 		text.add(makeLine("FNH", '${results.mc}'));
+		text.add("\n");
+
+		maxLineNumber = 0;
+		for (method in cls.getMethods()) {
+			var max:Int = reportBranches(text, method, branchCov);
+			if (max > maxLineNumber) {
+				maxLineNumber = max;
+			}
+		}
+		for (line in 0...maxLineNumber + 1) {
+			if (!branchCov.exists(line)) {
+				continue;
+			}
+			var count:String = branchCov.get(line);
+			text.add(makeLine("BRDA", '${line},${count}'));
+		}
+
 		text.add("\n");
 		text.add(makeLine("BRF", '${results.b}'));
 		text.add(makeLine("BRH", '${results.bt}'));
 		text.add("\n");
 
-		var lineCov:Map<Int, Int> = new Map<Int, Int>();
-		var maxLineNumber:Int = 0;
+		maxLineNumber = 0;
 		for (method in cls.getMethods()) {
-			var max:Int = reportMethod(text, method, lineCov);
+			var max:Int = reportStatements(text, method, lineCov);
 			if (max > maxLineNumber) {
 				maxLineNumber = max;
 			}
 		}
-
 		for (line in 0...maxLineNumber + 1) {
 			if (!lineCov.exists(line)) {
 				continue;
@@ -53,6 +86,7 @@ class LcovPrintClient implements CoverageReportClient {
 			var count:Int = lineCov.get(line);
 			text.add(makeLine("DA", '${line},${count}'));
 		}
+		text.add("\n");
 
 		text.add(makeLine("LF", '${results.l}'));
 		text.add(makeLine("LH", '${results.lc}'));
@@ -63,11 +97,8 @@ class LcovPrintClient implements CoverageReportClient {
 	}
 
 	@:access(mcover.coverage.data.Method)
-	function reportMethod(text:StringBuf, method:Method, lineCov:Map<Int, Int>):Int {
+	function reportStatements(text:StringBuf, method:Method, lineCov:Map<Int, Int>):Int {
 		var maxLineNumber:Int = 0;
-
-		// TODO get first line number of method
-		text.add(makeLine("FN", '1,${method.name}'));
 
 		for (statementId in method.statementsById.keys()) {
 			var statement:Statement = method.statementsById.get(statementId);
@@ -83,7 +114,6 @@ class LcovPrintClient implements CoverageReportClient {
 				}
 			} else {
 				for (line in branch.lines) {
-					text.add(makeLine("BRDA", '$line,${method.id},${branch.id},-'));
 					maxLineNumber = addLineCov(line, lineCov, branch.totalCount, maxLineNumber);
 				}
 			}
@@ -91,18 +121,34 @@ class LcovPrintClient implements CoverageReportClient {
 		return maxLineNumber;
 	}
 
-	function addLineCov(line:Int, lineCov:Map<Int, Int>, count:Int, maxLineNumber:Int):Int {
+	@:access(mcover.coverage.data.Method)
+	function reportBranches(text:StringBuf, method:Method, branchCov:Map<Int, String>):Int {
+		var maxLineNumber:Int = 0;
+
+		for (branchId in method.branchesById.keys()) {
+			var branch:Branch = method.branchesById.get(branchId);
+			var data:String = '${method.id},${branch.id},${branch.totalCount}';
+			if (branch.isCovered()) {
+				for (line in branch.lines) {
+					maxLineNumber = addLineCov(line, branchCov, data, maxLineNumber);
+				}
+			} else {
+				for (line in branch.lines) {
+					if (branch.isPartiallyCovered()) {
+						data = '${method.id},${branch.id},-';
+					}
+					maxLineNumber = addLineCov(line, branchCov, data, maxLineNumber);
+				}
+			}
+		}
+		return maxLineNumber;
+	}
+
+	function addLineCov<T>(line:Int, lineCov:Map<Int, T>, count:T, maxLineNumber:Int):Int {
 		if (line > maxLineNumber) {
 			maxLineNumber = line;
 		}
-		if (!lineCov.exists(line)) {
-			lineCov.set(line, count);
-			return maxLineNumber;
-		}
-		var old:Int = lineCov.get(line);
-		if (old > count) {
-			lineCov.set(line, count);
-		}
+		lineCov.set(line, count);
 		return maxLineNumber;
 	}
 
