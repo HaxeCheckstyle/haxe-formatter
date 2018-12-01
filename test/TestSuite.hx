1c927bd169d1c5c5ed3518ed0fe36374b309166d
AlexHaxe <Alexander.Blum@gmail.com>
2018-12-01 17:20:22 +0100

    removed CompileTime.getAllClasses since it uses Context.onMacroContextReused (see https://github.com/HaxeFoundation/haxe/issues/5746)

--- test/TestSuite.hx
+++ test/TestSuite.hx
@@ -1,4 +1,12 @@
 import formatter.FormatStatsTest;
+import testcases.EmptyLinesTestCases;
+import testcases.IndentationTestCases;
+import testcases.LineEndsTestCases;
+import testcases.MissingTestCases;
+import testcases.Other;
+import testcases.SameLineTestCases;
+import testcases.WhitespaceTestCases;
+import testcases.WrappingTestCases;
 
 class TestSuite extends massive.munit.TestSuite {
 	public function new() {
@@ -12,9 +20,18 @@ class TestSuite extends massive.munit.TestSuite {
 			add(FormatStatsTest);
 		}
 
-		var tests = CompileTime.getAllClasses(GoldBaseTest);
-		for (testClass in tests) {
-			add(testClass);
-		}
+		add(EmptyLinesTestCases);
+		add(IndentationTestCases);
+		add(LineEndsTestCases);
+		add(MissingTestCases);
+		add(Other);
+		add(SameLineTestCases);
+		add(WhitespaceTestCases);
+		add(WrappingTestCases);
+		// CompileTime.getAllClasses uses Context.onMacroContextReused which was removed from Haxe 4 (see https://github.com/HaxeFoundation/haxe/issues/5746)
+		// var tests = CompileTime.getAllClasses(GoldBaseTest);
+		// for (testClass in tests) {
+		// 	add(testClass);
+		// }
 	}
 }
