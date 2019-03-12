d233411f26f87fb46368edc9cb4da62a76edf37c
Jens Fischer <jensfischer95@gmail.com>
2019-03-13 00:31:48 +0100

    Avoid unnecessary dependencies when using FormatterConfig

--- src/formatter/import.hx
+++ src/formatter/import.hx
@@ -1,15 +1,17 @@
 package formatter;
 
+#if tokentree
 import byte.ByteData;
-import haxe.io.Bytes;
-import haxe.macro.Expr;
 import haxeparser.Data;
 import tokentree.TokenTree;
 import tokentree.TokenTreeAccessHelper;
 import tokentree.utils.TokenTreeCheckUtils;
+import haxe.io.Bytes;
+import haxe.macro.Expr;
 import formatter.codedata.ParsedCode;
 import formatter.codedata.TokenInfo;
 
 using StringTools;
 using tokentree.TokenTreeAccessHelper;
 using formatter.config.WhitespacePolicy;
+#end
