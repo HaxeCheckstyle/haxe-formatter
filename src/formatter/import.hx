ea65a24e8390155dfd78ea91d239353172090e7b
Jens Fischer <jensfischer95@gmail.com>
2019-03-13 00:34:24 +0100

    Restore order

--- src/formatter/import.hx
+++ src/formatter/import.hx
@@ -2,12 +2,12 @@ package formatter;
 
 #if tokentree
 import byte.ByteData;
+import haxe.io.Bytes;
+import haxe.macro.Expr;
 import haxeparser.Data;
 import tokentree.TokenTree;
 import tokentree.TokenTreeAccessHelper;
 import tokentree.utils.TokenTreeCheckUtils;
-import haxe.io.Bytes;
-import haxe.macro.Expr;
 import formatter.codedata.ParsedCode;
 import formatter.codedata.TokenInfo;
 
